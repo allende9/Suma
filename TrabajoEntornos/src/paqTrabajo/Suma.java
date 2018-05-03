3d6857bf6a789dd912cd8d8e987694b019ad648a
allende <daw@AFEQUIPO22>
2018-05-03 13:46:28 +0200

    Primera prueba

---
+++ TrabajoEntornos/src/paqTrabajo/Suma.java
@@ -0,0 +1,32 @@
+package paqTrabajo;
+
+import java.util.Scanner;
+
+public class Suma {
+
+	private int num1, num2, total;
+	
+	Scanner teclado = new Scanner(System.in);
+	
+	Suma(){
+		
+		System.out.println("Introduce un número");
+		num1 = teclado.nextInt();
+		System.out.println("Introduce otro número");
+		num2 = teclado.nextInt();
+		total = num1+num2;
+	}
+	
+	public void visualizar(){
+		
+		System.out.println("El resultado de la suma de " + num1 + " + " + num2 + " es de: " + total);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Suma obj = new Suma();
+		
+		obj.visualizar();
+	}
+
+}
